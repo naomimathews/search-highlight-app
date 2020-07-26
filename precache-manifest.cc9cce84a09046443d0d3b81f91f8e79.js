0c785125f0f3877ddbf45b2e3697f810c6279978
Naomi Mathews <naomi@defraglabs.co>
2020-07-26 23:55:34 +0530

    Updates

---
+++ precache-manifest.cc9cce84a09046443d0d3b81f91f8e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ed2892dc6f089f01c51736e849c286",
+    "url": "/search-highlight-app/index.html"
+  },
+  {
+    "revision": "759b24f2d61b0daebc6c",
+    "url": "/search-highlight-app/static/css/main.1fd29e48.chunk.css"
+  },
+  {
+    "revision": "300ed185f09becb4540d",
+    "url": "/search-highlight-app/static/js/2.fa6bf978.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-highlight-app/static/js/2.fa6bf978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759b24f2d61b0daebc6c",
+    "url": "/search-highlight-app/static/js/main.727c362e.chunk.js"
+  },
+  {
+    "revision": "64118e6af3b22519553a",
+    "url": "/search-highlight-app/static/js/runtime-main.bceb5b7e.js"
+  }
+]);
